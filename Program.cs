f85d312f6f3c32d227b76597c99ad8c9abaecb00
BorAlina2021 <alinaborachok2504@gmail.com>
2022-09-08 20:37:11 +0300

    Added new console writeline

--- Program.cs
+++ Program.cs
@@ -5,11 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("What is your name?");
-                var name = Console.ReadLine();
-                var currentDate = DateTime.Now;
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
+            Console.WriteLine("Vasyl changes");
         }
     }
 }
