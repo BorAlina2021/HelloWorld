8a067b670e87db883b312f19a6fdfab6e456abc8
Alina Borachok <90210466+BorAlina2021@users.noreply.github.com>
2022-09-12 20:32:07 +0300

    Swapnumbers (#6)
    * Added changes
    
    * added changes
    
    * Added changes
    
    * Code refactring

--- Program.cs
+++ Program.cs
@@ -4,36 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is your name?");
-            var name = Console.ReadLine();
-            name = "Alina";
-            var currentDate = DateTime.Now;
-            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            Console.WriteLine($"{Environment.NewLine}{currentDate:t} Where do you live now, {name}?");
-            Console.WriteLine($"{Environment.NewLine}{name}, explain me the difference between dogs and cats.");
-            Console.Write($"{Environment.NewLine}Press any key to exit...");
-            Console.ReadKey(true);
+            // Console.WriteLine("What is your name?");
+            // var name = Console.ReadLine();
+            // name = "Alina";
+            // var currentDate = DateTime.Now;
+            // Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            // Console.WriteLine($"{Environment.NewLine}{currentDate:t} Where do you live now, {name}?");
+            // Console.WriteLine($"{Environment.NewLine}{name}, explain me the difference between dogs and cats.");
+            // Console.Write($"{Environment.NewLine}Press any key to exit...");
+            // Console.ReadKey(true);
 
-            Console.WriteLine();
-            Console.WriteLine();
-            Console.WriteLine(MethodSum(4, 9));
-            Console.ReadLine();
-            Console.WriteLine(MethodDivision(16.56, 3.13));
-            Console.WriteLine();
+            // Console.WriteLine();
+            // Console.WriteLine();
+            // Console.WriteLine(MethodSum(4, 9));
+            // Console.ReadLine();
+            // Console.WriteLine(MethodDivision(16.56, 3.13));
+            // Console.WriteLine();
 
             int firstNum = 5;
             int secondNum = 6;
             Console.WriteLine("Value of FirstNum and SecondNum before swapping:");
             Console.WriteLine();
-            Console.WriteLine("firstNum=" + " " + firstNum);
-            Console.WriteLine("secondNum=" + " " + secondNum);
             SwapNum(firstNum, secondNum);
-            Console.WriteLine();
-            Console.WriteLine("Value of FirstNum and SecondNum after swapping:");
-            Console.WriteLine();
-            Console.WriteLine("firstNum=" + " " + secondNum);
-            Console.WriteLine("secondNum=" + " " + firstNum);
-            Console.WriteLine();
         }
 
         static int MethodSum(int x, int y)
@@ -48,9 +40,14 @@
 
         static void SwapNum(int firstNum, int secondNum)
         {
+            Console.WriteLine("firstNum=" + " " + firstNum);
+            Console.WriteLine("secondNum=" + " " + secondNum);
             int g = firstNum;
             firstNum = secondNum;
-            firstNum = g;
+            secondNum = g;
+            Console.WriteLine("Value of FirstNum and SecondNum after swapping:");
+            Console.WriteLine("firstNum=" + " " + firstNum);
+            Console.WriteLine("secondNum=" + " " + secondNum);
         }
     }
 }
