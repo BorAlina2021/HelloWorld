51f53c3ad2c1ba98700ab16ec4974b5f91f4dedf
BorAlina2021 <alinaborachok2504@gmail.com>
2022-09-12 16:50:11 +0300

    Added changes

--- Program.cs
+++ Program.cs
@@ -6,23 +6,22 @@
         {
             Console.WriteLine("What is your name?");
             var name = Console.ReadLine();
+            name = "Alina";
             var currentDate = DateTime.Now;
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            Console.WriteLine($"{Environment.NewLine}{currentDate:t}Where do you live now, {name}?");
+            Console.WriteLine($"{Environment.NewLine}{currentDate:t} Where do you live now, {name}?");
             Console.WriteLine($"{Environment.NewLine}{name}, explain me the difference between dogs and cats.");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
-            Console.WriteLine("Vasyl changes");
+
+            Console.ReadLine();
+            Console.WriteLine(MethodSum(4, 9));
+
         }
 
         static int MethodSum(int x, int y)
         {
             return x + y;
-        }
-
-        static void MainSumMethod(string[] args)
-        {
-            Console.WriteLine(MethodSum(4, 9));
         }
     }
 }
