65efcca2624f25c797833f3ea56dce28ae579135
BorAlina2021 <alinaborachok2504@gmail.com>
2022-09-12 11:53:20 +0300

    Console.WriteLine

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
             var name = Console.ReadLine();
             var currentDate = DateTime.Now;
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.WriteLine($"{Environment.NewLine}{currentDate:t}Where do you live now, {name}?");
             Console.Write($"{Environment.NewLine}Press any key to exit...");
             Console.ReadKey(true);
             Console.WriteLine("Vasyl changes");
